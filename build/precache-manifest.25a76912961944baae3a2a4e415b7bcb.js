84273a089a4d813d57382500dac57e4a78f8dd19
smdurjoy <smdurjoy963@gmail.com>
2020-05-13 15:06:51 +0600

    Initial Commit Frontend

---
+++ build/precache-manifest.25a76912961944baae3a2a4e415b7bcb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24666a1079c0a1a71e2f06e4ce6f4d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6377bd06f2a9138bb677",
+    "url": "/static/css/2.0ebad843.chunk.css"
+  },
+  {
+    "revision": "078f2cf0bff5fc06d656",
+    "url": "/static/css/main.df10bcb2.chunk.css"
+  },
+  {
+    "revision": "6377bd06f2a9138bb677",
+    "url": "/static/js/2.da3b36d6.chunk.js"
+  },
+  {
+    "revision": "078f2cf0bff5fc06d656",
+    "url": "/static/js/main.29b29766.chunk.js"
+  },
+  {
+    "revision": "378be0eab5cddffc5e87",
+    "url": "/static/js/runtime-main.862645ae.js"
+  },
+  {
+    "revision": "8139e3596c260552796e4be3c043aa48",
+    "url": "/static/media/banner.8139e359.jpg"
+  },
+  {
+    "revision": "2a87eedaaa18e483ba04f0e565df1a1f",
+    "url": "/static/media/graphics.2a87eeda.svg"
+  },
+  {
+    "revision": "704e74849e80be9cdc356d5f1c2ea050",
+    "url": "/static/media/mobile.704e7484.svg"
+  },
+  {
+    "revision": "f599bd96d0ddf7dcec79676b24f20962",
+    "url": "/static/media/navlogo.f599bd96.svg"
+  },
+  {
+    "revision": "d6db46a970f10d7612213d5e4ed72ed7",
+    "url": "/static/media/navlogoScroll.d6db46a9.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "3515765f8235b5f2fe302334ba570cc5",
+    "url": "/static/media/web.3515765f.svg"
+  }
+]);
