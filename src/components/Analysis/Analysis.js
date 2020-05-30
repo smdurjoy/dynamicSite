acc6d65bc0b31701effa4e0bec5138e07ba05b71
smdurjoy <smdurjoy963@gmail.com>
2020-05-30 20:19:06 +0600

    Rest API Consuming

--- src/components/Analysis/Analysis.js
+++ src/components/Analysis/Analysis.js
@@ -1,53 +1,57 @@
 import React, {Component,Fragment} from 'react';
 import {Col, Container, Row} from "react-bootstrap";
 import {BarChart, Bar, ResponsiveContainer, XAxis, Tooltip} from "recharts";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
+import ReactHtmlParser from 'react-html-parser';
 
 class Analysis extends Component {
 
     constructor(){
         super();
         this.state={
-            data:[
-                {Technology:'Java', Projects:100},
-                {Technology:'Kotlin', Projects:40},
-                {Technology:'Sql', Projects:90},
-                {Technology:'Bootstrap', Projects:95},
-                {Technology:'Jquery', Projects:60},
-                {Technology:'React', Projects:90},
-                {Technology:'PHP', Projects:100},
-                {Technology:'Angular', Projects:65}
-            ]
+            data:[],
+            techDescription: ''
         }
     }
 
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.ChartData).then(result => {
+            this.setState({data:result})
+        }).catch(error => {
+
+        })
+
+        RestClient.GetRequest(AppUrl.TechDesc).then(result => {
+            this.setState({techDescription:result[0]['tech_description']})
+        }).catch(error => {
+
+        })
+    }
 
     render() {
-        var blue="rgba(0,115,230,0.8)"
+        let blue="rgba(0,115,230,0.8)"
         return (
             <Fragment>
                 <Container className="text-center">
                     <h1 className="serviceMainTitle">Technology Used</h1>
                     <Row>
                         <Col style={{width:'100%', height:'300px'}} lg={6} md={12} sm={12}>
-
                             <ResponsiveContainer>
                                 <BarChart width={100} height={300} data={this.state.data}>
-                                    <XAxis dataKey="Technology"/>
+                                    <XAxis dataKey="technology"/>
                                     <Tooltip/>
-                                    <Bar dataKey="Projects" fill={blue} >
+                                    <Bar dataKey="skill" fill={blue} >
                                     </Bar>
                                 </BarChart>
                             </ResponsiveContainer>
-
-
                         </Col>
+
                         <Col lg={6} md={12} sm={12}>
-                        <p className="text-justify des">To build native android apps i use Java as well as kotline mainly. React JS is used for cross platform mobile application. I use MySQL database for relational database system. To build NoSQL application i use MongoDB. Firebase database system is used where it is necessary to provide realtime data flow facilities.</p>
-                            <p className="text-justify des"> I always build dynamic application. Admin panel is the heart of such kinds of application. I always try to provide sufficient features in admin panel to dominate application. According to client demand I use PHP OOP, CodeIgniter and Laravel to build admin panel section. </p>
-                            <p className="text-justify des"> Application security is a big deal for commercial application. I always ensure security portion of my application, moreover i build a security alert system, to notify admin when his system at risk. </p>
-
+                        <p className="text-justify des">
+                            { ReactHtmlParser(this.state.techDescription) }
+                        </p>
                         </Col>
-
                     </Row>
                 </Container>
             </Fragment>
--- src/components/PrivacyDescription/PrivacyDescription.js
+++ src/components/PrivacyDescription/PrivacyDescription.js
@@ -1,7 +1,25 @@
 import React, {Component, Fragment} from 'react';
 import {Col, Container, Row} from "react-bootstrap";
+import RestClient from "../../RestAPI/RestClient";
+import AppUrl from "../../RestAPI/AppUrl";
+import ReactHtmlParser from 'react-html-parser';
 
 class PrivacyDescription extends Component {
+    constructor() {
+        super();
+        this.state = {
+            privacyDes: ""
+        }
+    }
+
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.Information).then(result => {
+            this.setState({privacyDes: result[0]['privacy']})
+        }).catch(err => {
+
+        })
+    }
+
     render() {
         return (
             <Fragment>
@@ -9,7 +27,7 @@ class PrivacyDescription extends Component {
                     <Row>
                         <Col sm={12} lg={12} md={12}>
                             <p className="serviceDescription">
-                                Unless you have entered into an express written contract with this website to the contrary, visitors, viewers, subscribers, members, affiliates, or customers have no right to use this information in a commercial or public setting; they have no right to broadcast it, copy it, save it, print it, sell it, or publish any portions of the content of this website. By accessing the contents of this website, you agree to this condition of access and you acknowledge that any unauthorized use is unlawful and may subject you to civil or criminal penalties. Again, Visitor has no rights whatsoever to use the content of, or portions thereof, including its databases, invisible pages, linked pages, underlying code, or other intellectual property the site may contain, for any reason or for any use whatsoever. In recognition of the fact that it may be difficult to quantify the exact damages arising from infringement of this provision, Visitor agrees to compensate the owners of this site with liquidated damages in the amount of U.S. $100,000, or, if it can be calculated, the actual costs and actual damages for breach of this provision, whichever is greater. Visitor warrants that he or she understands that accepting this provision is a condition of accessing this site and that accessing this site constitutes acceptance.
+                                { ReactHtmlParser(this.state.privacyDes) }
                             </p>
                         </Col>
                     </Row>
